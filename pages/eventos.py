05e2eaf44a877e06dcf2f79ed53441dd266ceacf
mawerneck <moisesaw@dcc.ufrj.br>
2025-11-20 21:36:07 -0300

    modified:   pages/eventos.py

--- pages/eventos.py
+++ pages/eventos.py
@@ -2,16 +2,9 @@ import streamlit as st
 import mysql.connector
 import pandas as pd
 from mysql.connector import Error
+from db import get_connection
 import os
 from dotenv import load_dotenv
-
-def get_connection():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
 
 st.title("Eventos")
 
